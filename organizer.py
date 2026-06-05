19fce85d47d6d66c1a8facda0a7c7ec72905e13e
tommw137 <tommw137@gmail.com>
2026-06-05 16:37:22 -0600

    Update organizer.py

--- organizer.py
+++ organizer.py
@@ -1,3 +1,5 @@
 import os
 def create_folder_structure(entity_name, category):
   os.makedirs(f"output/{entity_name}/{category}", exist_ok=True)
+def build_filename(name, document_type, extension):
+return f"{name}_{document_type}.{extension}"

2d72d299334f73b9ae466295b8a32839afb7439a
tommw137 <tommw137@gmail.com>
2026-06-09 15:01:04 -0600

    Update sumsub_client.py

--- sumsub_client.py
+++ sumsub_client.py
@@ -10,3 +10,9 @@ load_dotenv()
 APP_TOKEN = os.getenv("SUMSUB_APP_TOKEN")
 SECRET_KEY = os.getenv("SUMSUB_SECRET_KEY")
 BASE_URL = os.getenv("SUMSUB_BASE_URL")
+
+def sign_request(secret_key, method, url_path, body=b""):
+    timestamp = str(int(time.time()))
+    message = timestamp.encode() + method.encode() + url_path.encode() + body
+    signature = hmac.new(secret_key.encode(), message, hashlib.sha256).hexdigest()
+    return timestamp, signature
